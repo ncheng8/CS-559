98f0fb0502bca15a1c704f5e99c38969227cd21e
Nicholas Cheng <nickbaicheng@gmail.com>
2018-03-17 22:24:13 -0500

    Shader project initiate

--- p4/pfour.js
+++ p4/pfour.js
@@ -6,15 +6,28 @@ function setup() { "use strict";
   var slider1 = document.getElementById('slider1');
   slider1.value = 0;
   var slider2 = document.getElementById('slider2');
-  slider2.value = 0;
+  slider2.value = -5;
+  var slider3 = document.getElementById('slider3');
+  slider3.value = 0;
+  var slider4 = document.getElementById('slider4');
+  slider4.value = 1;
       
   var triangles = [];
 
-  function initGeometry(){
-    triangles.push([[100,100,100],[100,100,300],[100,300,300],"red",0.0]);
-    triangles.push([[100,100,100],[100,300,300],[100,300,100],"blue",0.0]);
-    triangles.push([[300,100,100],[300,100,300],[300,300,300],"green",0.0]);
-    triangles.push([[300,100,100],[300,300,300],[300,300,100],"purple",0.0]);
+  function initPyramid1(){
+    triangles.push([[100,0,100],[100,0,-100],[0,150,0],"red", m4.identity,0.0,1]);
+    triangles.push([[100,0,-100],[-100,0,-100],[0,150,0],"yellow",m4.identity,0.0,1]);
+    triangles.push([[-100,0,-100],[-100,0,100],[0,150,0],"orange",m4.identity,0.0,1]);
+    triangles.push([[-100,0,100],[100,0,100],[0,150,0],"purple",m4.identity,0.0,1]);
+	triangles.push([[100,0,100],[-100,0,100],[-100,0,-100],"blue",m4.identity,0.0,1]);
+	triangles.push([[100,0,100],[100,0,-100],[-100,0,-100],"blue",m4.identity,0.0,1]);
+  }
+  
+  function initFlower(){
+    triangles.push([[0,0,0],[200,250,-50],[200,250,50],"red",m4.identity,0.0,2]);
+    triangles.push([[0,0,0],[-200,250,-50],[-200,250,50],"yellow",m4.identity,0.0,2]);
+    triangles.push([[0,0,0],[-50,250,200],[50,250,200],"orange",m4.identity,0.0,2]);
+    triangles.push([[0,0,0],[-50,250,-200],[50,250,-200],"purple",m4.identity,0.0,2]);
   }
                   
   function moveToTx(x,y,z,Tx) {
@@ -31,6 +44,7 @@ function setup() { "use strict";
                 
   function drawAxes(Tx) {
     // A little cross on the front face, for identification
+	context.strokeStyle = "black";
     moveToTx(0,0,0,Tx);lineToTx(100,0,0,Tx);context.stroke();
     moveToTx(0,0,0,Tx);lineToTx(0,150,0,Tx);context.stroke();
     moveToTx(0,0,0,Tx);lineToTx(0,0,200,Tx);context.stroke();
@@ -39,50 +53,50 @@ function setup() { "use strict";
   function drawTriangle(triangle,Tx){
      context.beginPath();
      context.fillStyle=triangle[3];
+	 context.strokeStyle = "black";
      moveToTx(triangle[0][0],triangle[0][1],triangle[0][2],Tx); 
      lineToTx(triangle[1][0],triangle[1][1],triangle[1][2],Tx); 
      lineToTx(triangle[2][0],triangle[2][1],triangle[2][2],Tx); 
+	 context.stroke();
      context.closePath(); 
      context.fill();
   }
-                  
-  function drawCube(Tx) {
-    // A little cross on the front face, for identification
-    moveToTx(180,200,100,Tx);lineToTx(220,200,100,Tx);context.stroke();
-    moveToTx(200,180,100,Tx);lineToTx(200,220,100,Tx);context.stroke();
-    // Twelve edges of a cube
-    moveToTx(100,100,100,Tx);lineToTx(300,100,100,Tx);
-    lineToTx(300,300,100,Tx);lineToTx(100,300,100,Tx);context.stroke();
-    moveToTx(300,100,100,Tx);lineToTx(300,100,300,Tx);
-    lineToTx(300,300,300,Tx);lineToTx(300,300,100,Tx);context.stroke();
-    moveToTx(300,100,300,Tx);lineToTx(100,100,300,Tx);
-    lineToTx(100,300,300,Tx);lineToTx(300,300,300,Tx);context.stroke();
-    moveToTx(100,100,300,Tx);lineToTx(100,100,100,Tx);
-    lineToTx(100,300,100,Tx);lineToTx(100,300,300,Tx);context.stroke();
-  }
 
-  function drawScene(Tx) {
+  function drawShapes() {
     for(var i=0;i<triangles.length;i++)
-       drawTriangle(triangles[i],Tx);
+		drawTriangle(triangles[i],triangles[i][4]);
   }
 
+  function sortShapes() {
+	for (var i=0;i<triangles.length;i++) {
+		for (var j=0;j<(triangles.length-i-1);j++) {
+			if (triangles[j][5] < triangles[j+1][5]) {
+				var temp = triangles[j];
+				triangles[j] = triangles[j+1];
+				triangles[j+1] = temp;
+			}
+		}	
+	}
+  }
+  
   function draw() {
-    // hack to clear the canvas fast
     canvas.width = canvas.width;
     
-    var angle1 = slider1.value*0.01*Math.PI;
-    var angle2 = slider2.value*0.01*Math.PI;
-    var axis = [1,1,1];
+    var a1 = slider1.value*0.01*Math.PI;
+    var a2 = slider2.value*0.01*Math.PI;
+	var a3 = slider3.value*0.01*Math.PI;
+	var z4 = slider4.value*0.05;
+    var axis = [0,0,1];
   
-    var Tmodel=m4.axisRotation(axis,angle2);
+    var Tmodel=m4.axisRotation(axis,a2);
 
-    var eye=[700*Math.cos(angle1),400,700*Math.sin(angle1)];
+    var eye=[700*Math.cos(a1),400,700*Math.sin(a1)];
     var target=[0,0,0];
     var up=[0,1,0];
     var Tcamera=m4.inverse(m4.lookAt(eye,target,up));
 
     //var Tprojection=m4.ortho(-250,250,-200,300,-2,2);
-    var Tprojection=m4.perspective(Math.PI/3,1,5,400);
+    var Tprojection=m4.perspective(Math.PI*z4,1,5,400);
 
     var Tviewport=m4.multiply(m4.scaling([200,-200,200]),m4.translation([200,200,0]));
     
@@ -90,19 +104,32 @@ function setup() { "use strict";
     var Tmcp=m4.multiply(m4.multiply(Tmodel,Tcamera),Tprojection);
     var Tmc=m4.multiply(Tmodel,Tcamera);
     var Tmcpv=m4.multiply(Tmodel,Tcpv);
+	var T1 = m4.multiply(m4.translation([0,150,0]),Tmcpv);
+	T1 = m4.multiply(m4.axisRotation(up,a3),T1);
 
     for(var i=0;i<triangles.length;i++){
-      var cam=m4.transformPoint(Tmc,triangles[i][1]);
-      triangles[i][3]="rgb("+Math.floor(-cam[2]*0.3)+",0,0)";
+	  var cam1=m4.transformPoint(Tmc,triangles[i][0]);
+	  var cam2=m4.transformPoint(Tmc,triangles[i][1]);
+	  var cam3=m4.transformPoint(Tmc,triangles[i][2]);
+	  triangles[i][5]=-(cam1[2] + cam2[2] + cam3[2] )/ 3;
+	  if (triangles[i][6] == 1) {
+		  triangles[i][4] = Tmcpv;
+	  } else if (triangles[i][6] == 2) {
+		  triangles[i][4] = T1;
+	  }
     }
-    drawScene(Tmcpv);
-    drawCube(Tmcpv);
+	
+	sortShapes();
+    drawShapes();
     drawAxes(Tcpv);
   }
 
   slider1.addEventListener("input",draw);
   slider2.addEventListener("input",draw);
-  initGeometry();
+  slider3.addEventListener("input",draw);
+  slider4.addEventListener("input",draw);
+  initPyramid1();
+  initFlower();
   draw();
 
 }
